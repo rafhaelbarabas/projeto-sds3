235327db6fe384844b81cce44fe961ad63024cdb
Rafhael de Andrade Barabas <e-barabas@weg.net>
2021-05-03 18:40:14 -0300

    project created

---
+++ backend/src/main/java/com/rafhael/barabas/dssales/DsSalesApplication.java
@@ -0,0 +1,13 @@
+package com.rafhael.barabas.dssales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsSalesApplication.class, args);
+	}
+
+}
