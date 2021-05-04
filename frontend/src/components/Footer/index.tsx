b83bca89da3c46340bba63e3551d440f61bd4b70
Rafhael de Andrade Barabas <e-barabas@weg.net>
2021-05-03 21:03:51 -0300

    basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/rafhaelbarabas" target="_blank" rel="noreferrer">Rafhael Andrade</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+// TODO: Adicionar icones com link para LinkedIn, Facebook, instagram e GitHub
+export default Footer;
