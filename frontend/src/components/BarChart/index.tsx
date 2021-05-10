ab552e0ab2152625319560bd40dd9b125d058482
Rafhael de Andrade Barabas <e-barabas@weg.net>
2021-05-09 22:40:54 -0300

    barchart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,16 +1,53 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+  name: string;
+  data: number[];
+}
 
 type ChartData = {
-  labels: string[];
-  series: number[];
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
 }
 
 function BarChart() {
 
-  let chartData: ChartData = {
-    labels: [],
-    series: []
-  };
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: []
+    },
+    series: [
+      { name: '', data: [] }
+    ]
+  })
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`)
+      .then(res => {
+        const data = res.data as SaleSuccess[];
+        const labels = data.map(x => x.sellerName);
+        const series = data.map(x => round(100.0 * x.deals / x.visited, 1));
+        setChartData({
+          labels: {
+            categories: labels
+          },
+          series: [
+            { name: '% Success', data: series }
+          ]
+        })
+      })
+      .catch(error => {
+        console.log('erro ao realizar a requisição: ' + error)
+      })
+  }, [])
 
   const options = {
     plotOptions: {
@@ -20,21 +57,9 @@ function BarChart() {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1]
-      }
-    ]
-  };
-
   return (
     <Chart
-      options={{ ...options, labels: chartData.labels }}
+      options={{ ...options, xaxis: chartData.labels }}
       series={chartData.series}
       type="bar"
       height="240"
